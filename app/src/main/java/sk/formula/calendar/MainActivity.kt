144169dc6b43e654242ce66eca1797fe0310208a
Kvakino <mikeknor2503@gmail.com>
2024-07-17 23:52:35 +0200

    layout gp - not centered, without list of gps as parameter

--- app/src/main/java/sk/formula/calendar/MainActivity.kt
+++ app/src/main/java/sk/formula/calendar/MainActivity.kt
@@ -1,9 +1,7 @@
 package sk.formula.calendar
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,17 +10,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -147,24 +138,21 @@ fun ScheduleRow(session: String, date: String, time: String) {
             fontSize = 15f,
             outlineColor = Color.Black,
             textColor = Color.White,
-            outlineThickness = 1f,
-            modifier = Modifier.padding(start = Dp(20f))
+            outlineThickness = 1f
         )
         OutlineText(
             text = date,
             fontSize = 15f,
             outlineColor = Color.Black,
             textColor = Color.White,
-            outlineThickness = 1f,
-            modifier = Modifier.padding(start = Dp(20f))
+            outlineThickness = 1f
         )
         OutlineText(
             text = time,
             fontSize = 15f,
             outlineColor = Color.Black,
             textColor = Color.White,
-            outlineThickness = 1f,
-            modifier = Modifier.padding(start = Dp(20f))
+            outlineThickness = 1f
         )
     }
 }
